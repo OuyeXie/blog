af745cfb74752ffc2bec339b4cf88db064f10129
Ouye Xie <ouyexie@amazon.com>
2017-06-29 16:39:45 -0700

    add records

---
+++ app/assets/javascripts/components/records2.js.jsx
@@ -0,0 +1,9 @@
+var Records2 = React.createClass({
+    render: function() {
+        return (
+            <div className="records">
+                <h2 className="title"> Records2 </h2>
+            </div>
+        );
+    }
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628231643) do
+ActiveRecord::Schema.define(version: 20170629231728) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20170628231643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
